5d9da723027a7c4b5c8cfdc92b4c4ef637d3a139
Rakeally <40327144+Rakeally@users.noreply.github.com>
2023-09-16 19:55:05 +0100

    completed giftlist and tested

--- client/index.js
+++ client/index.js
@@ -1,17 +1,44 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+async function fetchGift(name) {
+  try {
+    const merkleTree = new MerkleTree(niceList);
+    const index = niceList.findIndex((n) => n === name);
+
+    if (index === -1) {
+      throw new Error("Name not found in the nice list.");
+    }
+
+    const proof = merkleTree.getProof(index);
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      name,
+      proof,
+    });
+    return gift;
+  } catch (error) {
+    throw new Error(`Failed to fetch gift: ${error.message}`);
+  }
+}
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  try {
+    const [name] = process.argv.slice(2);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    if (!name) {
+      throw new Error(
+        `Please enter a name as a command-line argument. Ex ** node client/index "Everett Bergnaum DVM" **`
+      );
+    }
 
-  console.log({ gift });
+    const gift = await fetchGift(name);
+    console.log({ gift });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,26 +1,35 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const bodyParser = require("body-parser");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList);
+const root = merkleTree.getRoot();
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = root;
+app.post("/gift", (req, res) => {
+  try {
+    const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+    const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+    if (isInTheList) {
+      res.status(200).json({ message: "You got a toy robot!" });
+    } else {
+      res.status(200).json({ message: "You are not on the list :(" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
